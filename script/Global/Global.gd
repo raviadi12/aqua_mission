65b56ecdcb9a61fb665e41ca5dc123cb5ff9014f
Ravi Adi Prakoso <ravi.a.prakoso@gmail.com>
2025-11-18 09:38:36 +0700

    Add level

--- script/Global/Global.gd
+++ script/Global/Global.gd
@@ -4,16 +4,16 @@ extends Node
 const SCENE_PATH : Dictionary = {
 	"main_menu" = "uid://bn112riygqtb0",
 	"lvl_selection" = "uid://doac12axf5utw",
-	"lvl1" = "uid://dm1srs33un6cv",
-	"lvl2" = "",
-	"lvl3" = "",
-	"lvl4" = "",
-	"lvl5" = "",
-	"lvl6" = "",
-	"lvl7" = "",
-	"lvl8" = "",
-	"lvl9" = "",
-	"lvl10" = "",
+	"lvl1" = "res://scenes/2d_scenes/level1.tscn",
+	"lvl2" = "res://scenes/2d_scenes/level2.tscn",
+	"lvl3" = "res://scenes/2d_scenes/level3.tscn",
+	"lvl4" = "res://scenes/2d_scenes/level4.tscn",
+	"lvl5" = "res://scenes/2d_scenes/level5.tscn",
+	"lvl6" = "res://scenes/2d_scenes/level6.tscn",
+	"lvl7" = "res://scenes/2d_scenes/level7.tscn",
+	"lvl8" = "res://scenes/2d_scenes/level8.tscn",
+	"lvl9" = "res://scenes/2d_scenes/level9.tscn",
+	"lvl10" = "res://scenes/2d_scenes/level10.tscn",
 }
 
 #Global Function
